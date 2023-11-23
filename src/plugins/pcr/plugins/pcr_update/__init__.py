93720ecccb4deeb78692d3bb7716bf789567245c
liangzimiao <2968274386@qq.com>
2023-11-23 20:40:08 +0800

    优化更新插件

--- src/plugins/pcr/plugins/pcr_update/__init__.py
+++ src/plugins/pcr/plugins/pcr_update/__init__.py
@@ -1,16 +1,24 @@
-from arclet.alconna import Alconna
+from arclet.alconna import (
+    Alconna,
+    Arparma,
+    Args,
+    Option,
+    CommandMeta,
+    store_true,
+)
+
 
 from nonebot.plugin import PluginMetadata
-
+from nonebot import logger
 from nonebot.adapters.qq.event import GuildMessageEvent
 
 
-from nonebot_plugin_alconna import on_alconna
+from nonebot_plugin_alconna import AlconnaMatches, on_alconna
 from nonebot_plugin_apscheduler import scheduler
 
 from .config import Config, plugin_config
-from ...services.update_service import UpdateService
-
+from ...services import UpdateService
+from ...models import UpdateResult
 
 __plugin_meta__ = PluginMetadata(
     name="pcr_update",
@@ -26,100 +34,66 @@ __plugin_meta__ = PluginMetadata(
 update_service = UpdateService()
 
 
-cmd = Alconna("更新卡池")
-pcr_update_pool = on_alconna(
-    cmd,
-    aliases={"更新卡池"},
+strategy = ["strategy", "策略", "攻略"]
+pool = ["pool", "卡池", "pcr卡池", "PCR卡池"]
+chara = ["chara", "角色数据", "人物数据", "花名册"]
+
+alc = Alconna(
+    "更新",
+    Args["data", strategy + pool + chara],
+    Option("-F|-f|--force", action=store_true),
+    meta=CommandMeta(compact=True),
+)
+pcr_update = on_alconna(
+    alc,
+    auto_send_output=True,
+    aliases={"update", "重载"},
     priority=5,
-    use_origin=False,  # 是否使用未经 to_me 等处理过的消息
-    use_cmd_start=True,  # 是否使用 COMMAND_START 作为命令前缀
-    use_cmd_sep=True,  # 是否使用 COMMAND_SEP 作为命令分隔符
 )
 
 
-@pcr_update_pool.handle()
-async def update_pool(event: GuildMessageEvent):
+@pcr_update.handle()
+async def _(event: GuildMessageEvent, result: Arparma = AlconnaMatches()):
     """
-    手动更新卡池时试用此命令
+    处理PCR更新事件。
     """
-    ver = await update_service.update_pool()
-    await pcr_update_chara.finish(f"更新完成，当前卡池版号{ver}")
-
-
-cmd = Alconna("强制更新卡池")
-pcr_force_update_pool = on_alconna(
-    cmd,
-    aliases={"强制更新卡池"},
-    priority=5,
-    use_origin=False,  # 是否使用未经 to_me 等处理过的消息
-    use_cmd_start=True,  # 是否使用 COMMAND_START 作为命令前缀
-    use_cmd_sep=True,  # 是否使用 COMMAND_SEP 作为命令分隔符
-)
-
-
-@pcr_force_update_pool.handle()
-async def force_update_pool(event: GuildMessageEvent):
-    """
-    强制更新卡池
-    """
-    ver = await update_service.update_pool(force=True)
-    await pcr_update_chara.finish(f"更新完成，当前卡池版号{ver}")
-
-
-cmd = Alconna("更新角色数据")
-pcr_update_chara = on_alconna(
-    cmd,
-    aliases={
-        "重载花名册",
-        "更新角色数据",
-    },
-    priority=5,
-    use_origin=False,  # 是否使用未经 to_me 等处理过的消息
-    use_cmd_start=True,  # 是否使用 COMMAND_START 作为命令前缀
-    use_cmd_sep=True,  # 是否使用 COMMAND_SEP 作为命令分隔符
-)
-
-
-@pcr_update_chara.handle()
-async def update_chara(event: GuildMessageEvent):
-    """
-    手动更新角色数据
-    """
-    a, b, c, d = await update_service.update_pcr_data()
-    await pcr_update_chara.finish(
-        f"更新前:\nCHARA_NAME:{a}\nCHARA_PROFILE:{b}\n更新后:\nCHARA_NAME:{c}\nCHARA_PROFILE:{d}"
-    )
+    # print(result.find("force"))
+    if result.main_args["data"] in strategy:
+        pass
+    if result.main_args["data"] in pool:
+        res: UpdateResult = await update_service.update_pool(force=result.find("force"))
+        await pcr_update.finish(res.message)
+    if result.main_args["data"] in chara:
+        res: UpdateResult = await update_service.update_pcr_data()
+        await pcr_update.finish(res.message)
+    await pcr_update.finish("ok")
 
 
 if plugin_config.plugin_is_auto:
+    logger.info("定时更新PCR数据")
 
     @scheduler.scheduled_job("cron", hour="17", minute="05")
     async def update_data_sdj():
         """
         定时更新PCR数据
         """
-        await update_service.update_pool()
-        await update_service.update_pcr_data()
         if plugin_config.plugin_is_notice:
             pass
-        return
+        await update_service.update_pool()
+        await update_service.update_pcr_data()
 
 
-cmd = Alconna("查看PCR数据")
-pcr_get_data = on_alconna(
-    cmd,
+pcr_check_data = on_alconna(
+    "查看PCR数据",
     aliases={"查看数据"},
     priority=5,
-    use_origin=False,  # 是否使用未经 to_me 等处理过的消息
-    use_cmd_start=True,  # 是否使用 COMMAND_START 作为命令前缀
-    use_cmd_sep=True,  # 是否使用 COMMAND_SEP 作为命令分隔符
 )
 
 
-@pcr_get_data.handle()
+@pcr_check_data.handle()
 async def get_pcr_data(event: GuildMessageEvent):
     """
-    手动更新卡池时试用此命令
+    查看本地数据
     """
     a, b = await update_service.check_pcr_data()
-    await pcr_get_data.finish(f"CHARA_NAME:{a}\nCHARA_PROFILE:{b}")
+    await pcr_check_data.finish(f"CHARA_NAME:{a}\nCHARA_PROFILE:{b}")
