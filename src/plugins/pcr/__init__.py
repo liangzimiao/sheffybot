05a11e1337062519871cdf833f3e28941f59ed9b
liangzimiao <2968274386@qq.com>
2024-02-18 07:45:19 +0800

    统一配置文件

--- src/plugins/pcr/__init__.py
+++ src/plugins/pcr/__init__.py
@@ -12,6 +12,10 @@ __plugin_meta__ = PluginMetadata(
     usage="",
     config=Config,
 )
+# 环奈头像1701待修复
+# 设置UNKNOWN头像
+# 配置ffmpeg，否则无法发送m4a格式的语音
+
 
 driver = get_driver()
 
@@ -19,9 +23,9 @@ driver = get_driver()
 @driver.on_startup
 async def first_load():
     # 检查数据并补齐缺少的数据
-    await pcr_data.load_data()
-    # print(type(pcr_data.CHARA_NAME_ID["未知角色"]))
+    await pcr_data.load_pcr_data()
+    #print(type(pcr_data.CHARA_ROSTER.get("未知角色")))
+    #print(pcr_data.CHARA_ROSTER.get("未知角色"))
 
 
-# logger.debug(pcr_config)
 sub_plugins = load_plugins(str(Path(__file__).parent.joinpath("plugins").resolve()))
--- src/plugins/pcr/config.py
+++ src/plugins/pcr/config.py
@@ -5,25 +5,46 @@ from pydantic import BaseModel, Extra
 
 
 class Config(BaseModel, extra=Extra.ignore):
-    """Plugin Config Here"""
+    """PCR Plugin Config Here"""
 
-    # PCR 资源数据配置
+    # PCR 数据资源配置
     pcr_data_path: Path = Path(__file__).resolve().parent / "data"
     pcr_resources_path: Path = Path(__file__).resolve().parent / "resources"
     # PCR 运势配置
     pcr_portune_limit: int = 5
-    pcr_portune_is_reply: bool = False
+    """每日限制次数"""
+    pcr_portune_is_reply: bool = True
+    """是否启用回复"""
     # PCR 更新配置
     pcr_update_is_auto: bool = True
+    """是否自动更新卡池"""
     pcr_update_is_notice: bool = True
+    """是否通知卡池更新"""
     # PCR 抽卡配置
     pcr_gacha_cd: int = 0
     pcr_gacha_limit: int = 0
     # PCR WHOIS配置
-    pcr_whois_is_reply: bool = False
+    pcr_whois_is_reply: bool = True
+    """是否启用回复"""
     pcr_whois_cd: int = 0
+    """花名册间隔时间"""
     # PCR GUESS配置
     pcr_guess_is_reply: bool = False
+    """是否启用回复"""
+    pcr_avatar_patch_size: int = 32
+    """猜头像裁剪尺寸"""
+    pcr_avatar_one_turn_time: int = 20
+    """猜头像一轮时间"""
+    pcr_card_pic_side_length: int = 180
+    """猜卡面裁剪尺寸"""
+    pcr_card_one_turn_time: int = 20
+    """猜卡面一轮时间"""
+    pcr_desc_prepare_time: int = 5
+    """准备时间"""
+    pcr_desc_one_turn_time: int = 12
+    """每轮间隔时间"""
+    pcr_desc_turn_number: int = 5  # [<9]
+    """单次游戏轮数"""
 
 
 global_config = get_driver().config
