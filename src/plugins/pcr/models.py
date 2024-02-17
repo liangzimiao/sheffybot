c247843ac0bca6c64918c2c81d5aa2aa22cd0c68
liangzimiao <2968274386@qq.com>
2024-02-18 07:38:00 +0800

    models 模块化 删除多余的model

--- src/plugins/pcr/models.py
+++ src/plugins/pcr/models.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass, field
 from io import BytesIO
-from typing import Literal, Optional
+from typing import Any, Literal, Optional
 
 
 @dataclass
 class Chara:
+    """PCR角色"""
+
     id: str
     """角色id"""
     star: Literal[1, 3, 6]
@@ -22,3 +24,31 @@ class Chara:
         if isinstance(other, Chara):
             return self.id == other.id and self.name == other.name
         return False
+
+
+@dataclass
+class GuessGame:
+    """PCR GUESS游戏"""
+
+    gid: str
+    """参加游戏的群组GID"""
+    winner: Optional[str]
+    """胜利者ID"""
+    question: Any = field(repr=False)
+    """题目内容"""
+    answer: Chara
+    """答案角色"""
+
+
+@dataclass
+class WhoIsGuessResult:
+    """角色猜测结果"""
+
+    is_guess: bool
+    """是否是猜测结果"""
+    guess_name: str
+    """匹配到的名字"""
+    guess_chara: Chara
+    """匹配到的角色"""
+    score: int
+    """匹配度"""
