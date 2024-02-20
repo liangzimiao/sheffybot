81302ad9a2385cd4df528ef720b2b4da52dce686
liangzimiao <2968274386@qq.com>
2024-02-21 06:12:33 +0800

    add: pcr_gacha

---
+++ src/plugins/pcr/plugins/pcr_gacha.py
@@ -0,0 +1,202 @@
+from nonebot.plugin import PluginMetadata, on_command
+from nonebot_plugin_saa import Image, Mention, Text
+from nonebot_plugin_session import EventSession
+
+from ..services.gacha_service import Gacha, GachaService, logger
+
+__plugin_meta__ = PluginMetadata(
+    name="pcr_gacha",
+    description="""
+    pcr相关的抽卡模拟
+    """,
+    usage="[单抽|十连|来一井|查看卡池|切换卡池]",
+    config=None,
+)
+
+gacha_10_aliases = {
+    "抽十连",
+    "十连",
+    "十连！",
+    "十连抽",
+    "来个十连",
+    "来发十连",
+    "来次十连",
+    "抽个十连",
+    "抽发十连",
+    "抽次十连",
+    "十连扭蛋",
+    "扭蛋十连",
+    "10连",
+    "10连！",
+    "10连抽",
+    "来个10连",
+}
+gacha_1_aliases = [
+    "单抽",
+    "单抽！",
+    "来发单抽",
+    "来个单抽",
+    "来次单抽",
+    "扭蛋单抽",
+    "单抽扭蛋",
+]
+gacha_300_aliases = {"抽一井", "来一井", "来发井", "抽发井", "天井扭蛋", "扭蛋天井"}
+SUPER_LUCKY_LINE = 170
+
+gacha_service = GachaService()
+
+
+matcher = on_command("单抽", aliases=set(gacha_1_aliases), priority=5)
+
+
+@matcher.handle()
+async def _(session: EventSession):
+    # 获取群组id and uid
+    uid = session.id1
+    gid = session.id2
+    if gid is None or uid is None:
+        return
+    # 获取群组对应卡池
+    gacha = await gacha_service.get_gacha(gid=gid)
+    # 单抽
+    c, h = await gacha.gacha_one(gacha.up_prob, gacha.s3_prob, gacha.s2_prob)
+    # 构造消息
+    assert c.icon
+    msg = (
+        Mention(uid)
+        + Text(f'素敵な仲間が増えますよ！{c.name} {"★" * c.star}')
+        + Image(c.icon)
+    )
+    # 发送消息
+    await msg.send()
+
+
+matcher = on_command("十连", aliases=set(gacha_10_aliases), priority=5)
+
+
+@matcher.handle()
+async def _(session: EventSession):
+    # 获取群组id and uid
+    uid = session.id1
+    gid = session.id2
+    if gid is None or uid is None:
+        return
+    # 获取群组对应卡池
+    gacha = await gacha_service.get_gacha(gid=gid)
+    # 十连
+    c_list, hiishi = await gacha.gacha_ten()
+    # 构造消息
+    if hiishi >= SUPER_LUCKY_LINE:
+        msg = Text("恭喜海豹！おめでとうございます！")
+        await msg.send()
+    msg = Mention(uid) + Text("\n素敵な仲間が増えますよ！\n")
+    try:
+        img = await gacha_service.draw_gacha(c_list)
+        msg += Image(img)
+    except Exception as e:
+        logger.error(f"Draw_Gacha_Result error:{e}")
+    result = [f'{c.name}{"★" * c.star}' for c in c_list]
+    msg += Text(f"{' '.join(result[0:5])}")
+    msg += Text(f"\n{' '.join(result[5:])}")
+    # 发送消息
+    await msg.send()
+
+
+matcher = on_command("来一井", aliases=set(gacha_300_aliases), priority=5)
+
+
+@matcher.handle()
+async def _(session: EventSession):
+    # 获取群组id and uid
+    uid = session.id1
+    gid = session.id2
+    if gid is None or uid is None:
+        return
+    # 获取群组对应卡池
+    gacha: Gacha = await gacha_service.get_gacha(gid=gid)
+    # 来一井
+    result = await gacha.gacha_tenjou()
+    # 构造消息
+    s3 = len(result["s3"])
+    s2 = len(result["s2"])
+    s1 = len(result["s1"])
+    up = result["up_num"]
+    res = result["s3"]
+    length = len(res)
+    if length == 0:
+        msg = Text("竟...竟然没有3★？！")
+        await msg.send()
+    msg2 = Mention(uid) + Text("\n素敵な仲間が増えますよ！\n")
+    try:
+        if length != 0:
+            img = await gacha_service.draw_gacha(res)
+            msg2 += Image(img)
+    except Exception as e:
+        logger.error(f"Draw_Gacha_Result error:{e}")
+    msg = [
+        f"★★★×{s3} ★★×{s2} ★×{s1}",
+        f"\n获得{up}个up角色与女神秘石×{50*(s3) + 10*s2 + s1}！\n第{result['first_up_pos']}抽首次获得up角色\n"
+        if up
+        else f"\n获得女神秘石{50*(up+s3) + 10*s2 + s1}个！\n",
+    ]
+    if up == 0 and s3 == 0:
+        msg.append("太惨了，咱们还是退款删游吧...")
+    elif up == 0 and s3 > 7:
+        msg.append("up呢？我的up呢？")
+    elif up == 0 and s3 <= 3:
+        msg.append("这位酋长，大月卡考虑一下？")
+    elif up == 0:
+        msg.append("据说天井的概率只有12.16%")
+    elif up <= 2:
+        if result["first_up_pos"] < 50:
+            msg.append("你的喜悦我收到了，滚去喂鲨鱼吧！")
+        elif result["first_up_pos"] < 100:
+            msg.append("已经可以了，您已经很欧了")
+        elif result["first_up_pos"] > 290:
+            msg.append("标 准 结 局")
+        elif result["first_up_pos"] > 250:
+            msg.append("补井还是不补井，这是一个问题...")
+        else:
+            msg.append("期望之内，亚洲水平")
+    elif up == 3:
+        msg.append("抽井母五一气呵成！您就是欧洲人？")
+    elif up >= 4:
+        msg.append("记忆碎片一大堆！您是托吧？")
+    for i in msg:
+        msg2 += Text(i)
+    # 发送消息
+    await msg2.send()
+
+
+matcher = on_command("查看卡池", priority=5)
+
+
+@matcher.handle()
+async def _(session: EventSession):
+    # 获取群组id
+    gid = session.id2
+    # 获取群组对应卡池
+    pass
+    # 查看卡池
+    pass
+    # 构造消息
+    pass
+    # 发送消息
+    pass
+
+
+matcher = on_command("切换卡池", priority=5)
+
+
+@matcher.handle()
+async def _(session: EventSession):
+    # 获取群组id
+    gid = session.id2
+    # 获取群组对应卡池
+    pass
+    # 切换卡池
+    pass
+    # 构造消息
+    pass
+    # 发送消息
+    pass
---
+++ src/plugins/pcr/services/gacha_service.py
@@ -0,0 +1,279 @@
+import random
+import sqlite3
+from io import BytesIO
+from pathlib import Path
+from typing import List, Optional, Tuple
+
+from PIL import Image
+
+from ..config import pcr_config
+from ..logger import PCRLogger
+from ..models import Chara, GachaTenjouResult
+from .data_service import chara_data
+from .data_service import pcr_data as pcr
+
+pcr_res_path: Path = pcr_config.pcr_resources_path
+pcr_data_path: Path = pcr_config.pcr_data_path
+
+db_path = pcr_config.pcr_data_path / "gacha_game_data" / "pcr_gacha_gid.db"
+
+logger = PCRLogger("PCR_GACHA")
+
+
+class Dao:
+    def __init__(self, db_path: Path):
+        self.db_path = db_path
+        self.db_path.parent.mkdir(parents=True, exist_ok=True)
+        db_path.touch()
+        self._create_table()
+
+    def connect(self):
+        return sqlite3.connect(self.db_path)
+
+    def _create_table(self):
+        with self.connect() as conn:
+            conn.execute(
+                "CREATE TABLE IF NOT EXISTS gid_pool "
+                "(gid TEXT NOT NULL PRIMARY KEY, pool TEXT )"
+            )
+
+    def get_pool(self, gid: str) -> Optional[str]:
+        """
+        获取卡池
+        """
+        with self.connect() as conn:
+            r = conn.execute(
+                "SELECT pool FROM gid_pool WHERE gid=? ", (gid,)
+            ).fetchone()
+            return r[0] if r else None
+
+    def set_pool(self, gid: str, pool_name: str):
+        """
+        设置卡池
+        """
+        with self.connect() as conn:
+            conn.execute(
+                "INSERT OR REPLACE INTO gid_pool (gid, pool) VALUES (?, ?)",
+                (gid, pool_name),
+            )
+
+
+class Gacha:
+    def __init__(self, pool_name: str = "BL"):
+        pool = pcr.LOCAL_POOL[pool_name]
+        self.up_prob = pool["up_prob"]
+        self.s3_prob = pool["s3_prob"]
+        self.s2_prob = pool["s2_prob"]
+        self.s1_prob = 1000 - self.s2_prob - self.s3_prob
+        self.up = pool["up"]
+        self.star3 = pool["star3"]
+        self.star2 = pool["star2"]
+        self.star1 = pool["star1"]
+
+    async def gacha_one(
+        self,
+        up_prob: int,
+        s3_prob: int,
+        s2_prob: int,
+        icon: bool = True,
+    ) -> Tuple[Chara, int]:
+        """
+        sx_prob: x星概率，要求和为1000
+        up_prob: UP角色概率（从3星划出）
+        up_chara: UP角色名列表
+
+        return: (单抽结果:Chara, 秘石数:int)
+        ---------------------------
+        |up|      |  20  |   78   |
+        |   ***   |  **  |    *   |
+        ---------------------------
+        """
+        rn = random.SystemRandom()
+        pick = rn.randint(1, 1000)
+        if pick <= up_prob:
+            return await chara_data.get_chara(
+                name=rn.choice(self.up), star=3, need_icon=True
+            ), 100
+        elif pick <= s3_prob:
+            return await chara_data.get_chara(
+                name=rn.choice(self.star3), star=3, need_icon=True
+            ), 50
+        elif pick <= s2_prob + s3_prob:
+            return await chara_data.get_chara(
+                name=rn.choice(self.star2), star=2, need_icon=icon
+            ), 10
+        else:
+            return await chara_data.get_chara(
+                name=rn.choice(self.star1), star=1, need_icon=icon
+            ), 1
+
+    async def gacha_ten(self) -> Tuple[List[Chara], int]:
+        result = []
+        hiishi = 0
+        up = self.up_prob
+        s3 = self.s3_prob
+        s2 = self.s2_prob
+        for _ in range(9):  # 前9连
+            c, y = await self.gacha_one(up, s3, s2)
+            result.append(c)
+            hiishi += y if y != 100 else 50
+        c, y = await self.gacha_one(up, s3, 1000 - s3)  # 保底第10抽
+        result.append(c)
+        hiishi += y if y != 100 else 50
+
+        return result, hiishi
+
+    async def gacha_tenjou(self) -> GachaTenjouResult:
+        result: GachaTenjouResult = {
+            "s3": [],
+            "s2": [],
+            "s1": [],
+            "first_up_pos": 999,
+            "up_num": 0,
+            "hiishi": 0,
+        }
+        first_up_pos = 999
+        up_num = 0
+        hiishi = 0
+        num: int = 20
+        for i in range(num):
+            for j in range(1, 10):  # 前9连
+                c, y = await self.gacha_one(
+                    self.up_prob, self.s3_prob, self.s2_prob, icon=False
+                )
+                hiishi += y
+                if 100 == y:
+                    result["s3"].append(c)
+                    first_up_pos = min(i * 10 + j, first_up_pos)
+                    up_num += 1
+                elif 50 == y:
+                    result["s3"].append(c)
+                elif 10 == y:
+                    result["s2"].append(c)
+                elif 1 == y:
+                    result["s1"].append(c)
+            c, y = await self.gacha_one(
+                self.up_prob, self.s3_prob, 1000 - self.s3_prob, icon=False
+            )  # 保底第10抽
+            hiishi += y
+            if 100 == y:
+                result["s3"].append(c)
+                first_up_pos = min((i + 1) * 10, first_up_pos)
+                up_num += 1
+            elif 50 == y:
+                result["s3"].append(c)
+            elif 10 == y:
+                result["s2"].append(c)
+            elif 1 == y:
+                result["s1"].append(c)
+        result["first_up_pos"] = first_up_pos
+        result["up_num"] = up_num
+        result["hiishi"] = hiishi
+        return result
+
+
+class GachaService:
+    def __init__(self):
+        self.local_pool = pcr.LOCAL_POOL.copy()
+
+    async def draw_gacha(
+        self,
+        c_list: List[Chara],
+    ) -> BytesIO:
+        """
+        绘制抽卡结果
+        """
+        pics = []
+        step = 5
+        length = len(c_list)
+        for i in range(0, length, step):
+            j = min(length, i + step)
+            pics.append(gen_team_pic(c_list[i:j], star_slot_verbose=False))
+        res = concat_pic(pics)
+        bytes = BytesIO()
+        res.save(bytes, format="png")
+        bytes.seek(0)
+        return bytes
+
+    async def get_gacha(self, gid: str) -> Gacha:
+        """
+        获取群组对应的卡池
+        """
+        pool_name = self.db.get_pool(gid=gid)
+        if pool_name:
+            return Gacha(pool_name=pool_name)
+        return Gacha()
+
+    async def set_pool(self, gid: str, pool_name: str = "BL") -> None:
+        """
+        修改群组对应的卡池
+        """
+        self.db.set_pool(gid=gid, pool_name=pool_name)
+
+    def get_all_gacha(self):
+        pcr.LOCAL_POOL
+
+        return ""
+
+    @staticmethod
+    def get_gacha_name(name: str, id_: int) -> str:
+        pcr.LOCAL_POOL
+
+        return ""
+
+    @property
+    def db(self) -> Dao:
+        """
+        数据库对象。
+        """
+        return Dao(db_path)
+
+
+def concat_pic(pics: List[Image.Image], border=5):
+    num = len(pics)
+    w, h = pics[0].size
+    des = Image.new("RGBA", (w, num * h + (num - 1) * border), (255, 255, 255, 255))  # type: ignore
+    for i, pic in enumerate(pics):
+        des.paste(pic, (0, i * (h + border)), pic)
+    return des
+
+
+def gen_team_pic(team, size=64, star_slot_verbose=True) -> Image.Image:
+    num = len(team)
+    des = Image.new("RGBA", (num * size, size), color=(255, 255, 255, 255))  # type: ignore
+    for i, chara in enumerate(team):
+        src = render_icon(chara, size, star_slot_verbose)
+        des.paste(src, (i * size, 0), src)
+    return des
+
+
+def render_icon(c: Chara, size: int, star_slot_verbose: bool = True) -> Image.Image:
+    pic = (
+        Image.open(c.icon)
+        .convert("RGBA")
+        .resize((size, size), Image.Resampling.LANCZOS)
+    )
+    l_ = size // 6
+    star_lap = round(l_ * 0.15)
+    margin_x = (size - 6 * l_) // 2
+    margin_y = round(size * 0.05)
+    if c.star:
+        for i in range(5 if star_slot_verbose else min(c.star, 5)):
+            a = i * (l_ - star_lap) + margin_x
+            b = size - l_ - margin_y
+            s = pcr.gadget_star if c.star > i else pcr.gadget_star_dis
+            s = s.resize((l_, l_), Image.Resampling.LANCZOS)
+            pic.paste(s, (a, b, a + l_, b + l_), s)
+        if 6 == c.star:
+            a = 5 * (l_ - star_lap) + margin_x
+            b = size - l_ - margin_y
+            s = pcr.gadget_star_pink
+            s = s.resize((l_, l_), Image.Resampling.LANCZOS)
+            pic.paste(s, (a, b, a + l_, b + l_), s)
+    if c.equip:
+        l_ = round(l_ * 1.5)
+        a = margin_x
+        b = margin_x
+        s = pcr.gadget_equip.resize((l_, l_), Image.Resampling.LANCZOS)
+        pic.paste(s, (a, b, a + l_, b + l_), s)
+    return pic
